e09bee02282b993c9b6e114202420a1a8812e71c
angelxescomx08 <ah@xdevelop.mx>
2022-06-30 20:43:04 -0500

    estilos en pokemon page

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,20 +1,77 @@
+import { Button, Card, Container, Grid, Text, Image } from "@nextui-org/react";
 import { GetStaticProps, NextPage, GetStaticPaths } from "next";
 import { useRouter } from "next/router"
 import { pokeApi } from "../../api";
 import { Layout } from "../../components/layouts"
 import { Pokemon } from '../../interfaces';
 
-interface Props{
+interface Props {
   pokemon: Pokemon
 }
 
-const PokemonPage: NextPage<Props> = ({pokemon}) => {
+const PokemonPage: NextPage<Props> = ({ pokemon }) => {
 
   const router = useRouter();
 
   return (
     <Layout>
-        <h1>{pokemon.name}</h1>
+      <Grid.Container css={{ marginTop: '5px' }} gap={2}>
+        <Grid xs={12} sm={4}>
+          <Card isHoverable css={{ padding: '30px' }}>
+            <Card.Body>
+              <Card.Image
+                src={pokemon.sprites.other?.dream_world.front_default || '/no-iage.png'}
+                alt={pokemon.name}
+                width="100%"
+                height={200}>
+              </Card.Image>
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={12} sm={8}>
+          <Card>
+            <Card.Header css={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Text h1 transform="capitalize">{pokemon.name}</Text>
+              <Button
+                color='gradient'
+                ghost
+              >
+                Guardar en favoritos
+              </Button>
+            </Card.Header>
+            <Card.Body>
+              <Text size={30}>Sprites:</Text>
+              <Container display="flex" direction="row">
+                <Image
+                  src={pokemon.sprites.front_default}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.back_default}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.front_shiny}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.back_shiny}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+              </Container>
+            </Card.Body>
+          </Card>
+
+        </Grid>
+      </Grid.Container>
     </Layout>
   )
 }
@@ -24,18 +81,18 @@ const PokemonPage: NextPage<Props> = ({pokemon}) => {
  */
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
-  const pokemon151 = [...Array(151)].map((value,index)=> `${index+1}`)
+  const pokemon151 = [...Array(151)].map((value, index) => `${index + 1}`)
   return {
-    paths: pokemon151.map(id=>({
-      params: {id}
+    paths: pokemon151.map(id => ({
+      params: { id }
     })),
     fallback: false
   }
 }
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
-  const {id} = params as {id:string};
-  const {data} = await pokeApi.get<Pokemon>(`/pokemon/${id}`)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { id } = params as { id: string };
+  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${id}`)
   return {
     props: {
       pokemon: data
