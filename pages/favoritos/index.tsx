cc09a316b04821b5365b1dc7372e6f93c8ab8f1d
angelxescomx08 <ah@xdevelop.mx>
2022-07-01 20:59:09 -0500

    pagina de favoritos

---
+++ pages/favoritos/index.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../../components/layouts"
+
+const Favoritos = () => {
+  return (
+    <Layout title='pokemons favoritos'>
+      <h1>Favoritos</h1>
+    </Layout>
+  )
+}
+
+export default Favoritos
